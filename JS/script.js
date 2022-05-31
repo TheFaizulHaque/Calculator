457a8ea8159b97103fdca28e7cf92e39480a2c01
Faizul <faizul.khan01@gmail.com>
2022-06-01 00:19:46 +0530

    initial commit

---
+++ JS/script.js
@@ -0,0 +1,48 @@
+let outPut = document.getElementsByClassName('output');
+let buttons = Array.from(document.querySelectorAll('button'));
+console.log(buttons);
+// let equal = document.getElementsByClassName('equal');
+// let clear = document.getElementById('clear');
+// let outPutValue = '';
+
+// buttons.map( button => {
+//     button.addEventListener('click', (e) => {
+//         switch(e.target.innerText) {
+//             default:
+//                 outPut.innerText += e.target.innerText;  
+//         //     case value:
+                
+//         //         break;
+        
+//         //     default:
+//         //         break;
+//          }
+//     });
+// });
+ 
+
+
+
+
+
+
+
+
+
+// buttons.forEach(button => {
+//     button.addEventListener('click',(e) => {
+//         buttonText = e.target.innerText;
+//         outPut[0].textContent+=buttonText;
+//         //outPut[0].textContent = outPut[0].textContent + buttonText;
+
+//         // let equal= eval(buttonText);
+//         // document.getElementsByClassName("output").value= equal;
+
+       
+//            if (buttonText == "equal"){
+//            outPut = eval(outPut);    
+//            };
+//        });
+    
+//     });
+
